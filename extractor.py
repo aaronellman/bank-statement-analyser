9494e61561672fff35c779bdec393f57139ec1ba
aaronellman@gmail.com <aaronellman@gmail.com>
2026-04-07 00:05:13 +0200

    add: format_tables for db compatibility

--- extractor.py
+++ extractor.py
@@ -2,6 +2,8 @@ import pymupdf
 from pathlib import Path
 
 def get_tables(path: str):
+    """returns markdown of the tables in the statement and the tables themselves"""
+
     p = Path(path)
     files = list(p.iterdir()) if p.is_dir() else [p]
 
@@ -12,5 +14,18 @@ def get_tables(path: str):
         for page in doc:
             tables += page.find_tables()
 
-    return [table.to_markdown() for table in tables]
+    return ([table.to_markdown() for table in tables], tables)
 
+
+def format_tables(tables):
+    """takes the tables that get_tables returns and returning a list of dictionaries for insertion into db"""
+    
+    for table in tables:
+        if "Date" in table.extract()[0] and "Amount" in table.extract()[0]:
+            table_data = table.extract()
+
+            headers = table_data[0]
+            list_of_dicts = [dict(zip(headers, row)) for row in table_data[1:]]
+            print(list_of_dicts)
+
+    return list_of_dicts
--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 from rich.markdown import Markdown
-from extractor import get_tables
+from extractor import get_tables, format_tables
 
 def main():
     console = Console()
     console.rule("[bold]Bank-statement-analyzer")
-    tables = get_tables(input("Path to file: "))
+    
+    result = get_tables(input("Path to file: "))
+
+    tables_md, tables = result[0], result[1]
+    
+    formatted_result = format_tables(tables)
 
     print(f"[blue bold]Total Pages: {len(tables)}")
+
+    print(formatted_result)
     
-    for table in tables: 
+    for table in tables_md: 
         md = Markdown(table)
         console.print(md)
 
